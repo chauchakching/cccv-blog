c51dcbf81b5e976b9aeba83bc042f62af03a6da3
chauchakching <chauchakching@gmail.com>
2021-09-04 01:25:52 +0800

    remove twitter data, add github link

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,10 @@ module.exports = {
       // summary: `who lives and works in San Francisco building useful things.`,
     },
     // description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    // social: {
-    //   twitter: `kylemathews`,
-    // },
+    siteUrl: `https://cccv.blog/`,
+    social: {
+      github: 'https://github.com/chauchakching',
+    },
   },
   plugins: [
     `gatsby-plugin-image`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,21 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Layout = ({ location, title, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          social {
+            github
+          }
+        }
+      }
+    }
+  `)
+  const social = data.site.siteMetadata?.social
+
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
@@ -22,7 +36,18 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+      <header className="global-header">
+        {header}
+        <Link to={social.github}>
+          <StaticImage
+            className="github-avatar"
+            formats={["AUTO", "WEBP", "AVIF"]}
+            src="../images/GitHub-Mark-120px-plus.png"
+            width={34}
+            alt="github"
+          />
+        </Link>
+      </header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
