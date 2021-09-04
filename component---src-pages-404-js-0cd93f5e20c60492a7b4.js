d3cfa811ab6e44e741808666ecf10eb85be01119
chauchakching <chauchakching@gmail.com>
2021-09-04 16:34:16 +0000

    Deploying to gh-pages from @ chauchakching/cccv-blog@d85af22d8c3cf57b5c2d15e630784492f9824856 🚀

---
+++ component---src-pages-404-js-0cd93f5e20c60492a7b4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcccv_blog=self.webpackChunkcccv_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),l=a(7198),c=a(3751);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(c.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0cd93f5e20c60492a7b4.js.map
